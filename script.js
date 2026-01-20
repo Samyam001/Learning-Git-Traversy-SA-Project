425d38ae95af9d3f3b28b9594ae1f1125dc909ff
Samyam <shresthasamyam001@gmail.com>
2026-01-20 20:42:11 +0545

    HE

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 console.log("Hello, World!");
+
+user = "Samyam";
+
+let school="XYZ High School";
+console.log("School:",school);
+
+ age=25;
+
+
+ const student={
+    name:"Samyam",
+    age:25,
+    grade:"A"
+ }
+
+ student["name"]="Jo";
+ console.log("Student:",student);
+ console.log("Name:",student.name);
+
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+
+function age(year) {
+    return 2026 - year;
+}
+
+isFollowed = true;
+console.log("Followed:",isFollowed);
+
+console.log(greet(user));
+console.log(`You are ${age} years old.`);
+
+{
+const city = "New York";
+console.log("City:", city);
+}
+{
+    let city="kathman";
+    console.log("city: ",city);
+}
