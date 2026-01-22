d93314fd8fb4df2d1899101dee4e3d87a11aa20a
Samyam <shresthasamyam001@gmail.com>
2026-01-22 17:07:49 +0545

    HELLLLPPI

--- script.js
+++ script.js
@@ -1,43 +1,12 @@
-console.log("Hello, World!");
+let userName = prompt("Enter your name:");
+alert("Hello, " + userName + "!");
 
-user = "Samyam";
-
-let school="XYZ High School";
-console.log("School:",school);
-
- age=25;
+const inputField = document.getElementById("userInput");
+const submitButton = document.getElementById("submitBtn");
+const outputDiv = document.getElementById("output");
 
 
- const student={
-    name:"Samyam",
-    age:25,
-    grade:"A"
- }
-
- student["name"]="Jo";
- console.log("Student:",student);
- console.log("Name:",student.name);
-
-function greet(name) {
-    return `Hello, ${name}!`;
-}
-
-
-function age(year) {
-    return 2026 - year;
-}
-
-isFollowed = true;
-console.log("Followed:",isFollowed);
-
-console.log(greet(user));
-console.log(`You are ${age} years old.`);
-
-{
-const city = "New York";
-console.log("City:", city);
-}
-{
-    let city="kathman";
-    console.log("city: ",city);
-}
+submitButton.addEventListener("click", function() {
+    const userInput = inputField.value;
+    outputDiv.innerText = "You entered: " + userInput;
+});
